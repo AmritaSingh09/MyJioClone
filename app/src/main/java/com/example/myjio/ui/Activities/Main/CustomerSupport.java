502dcc0c6aed78cd3b051bf85701e6c047a2a22c
AmritaSingh09 <amritasingh09092002@gmail.com>
2023-09-20 17:32:31 +0530

    UI update on Music.java (1)

---
+++ app/src/main/java/com/example/myjio/ui/Activities/Main/CustomerSupport.java
@@ -0,0 +1,17 @@
+package com.example.myjio.ui.Activities.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myjio.R;
+
+public class CustomerSupport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_support);
+
+    }
+}
--- app/src/main/java/com/example/myjio/ui/Activities/Main/MainActivity.java
+++ app/src/main/java/com/example/myjio/ui/Activities/Main/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myjio.ui.Activities.Main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -13,20 +16,35 @@ import com.example.myjio.core.Adapters.SliderHome1;
 
 public class MainActivity extends AppCompatActivity {
 
+    //bottom container views
+    private LinearLayout v_home, v_recharge, v_music, v_care, v_menu;
+
+    //top container views
+    private ImageView v_qr, v_voice;
+    private RelativeLayout v_notification, v_search;
+    private TextView v_txt_notification, v_search_txt;
+
+    //middle container views
     private ViewPager top_slider;
     private LinearLayout top_dot_slider;
+
+    //global variables
     private int dotscount;
     private ImageView[] dots;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Integer[] images = {R.drawable.img_banner_comming_soon, R.drawable.img_banner_computer, R.drawable.img_banner_sale, R.drawable.img_banner_sale_80off};
+        initViews();
 
-        top_slider = findViewById(R.id.top_slider);
-        top_dot_slider = findViewById(R.id.top_dot_slider);
+        v_care.setOnClickListener(care_view ->  startActivity(new Intent(MainActivity.this, CustomerSupport.class)));
+        v_music.setOnClickListener(care_view ->  startActivity(new Intent(MainActivity.this, Music.class)));
+        v_recharge.setOnClickListener(care_view ->  startActivity(new Intent(MainActivity.this, Recharge.class)));
+
+        Integer[] images = {R.drawable.img_banner_comming_soon, R.drawable.img_banner_computer, R.drawable.img_banner_sale, R.drawable.img_banner_sale_80off};
 
         SliderHome1 top_slider_adapter = new SliderHome1(this,images);
         top_slider.setClipToPadding(false);
@@ -72,6 +90,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private synchronized void initViews() {
+        // top views
+        v_qr = findViewById(R.id.home_qr_icon);
+        v_notification = findViewById(R.id.home_notification);
+        v_txt_notification = findViewById(R.id.no_of_notification);
+        v_search = findViewById(R.id.home_search_view);
+        v_voice = findViewById(R.id.home_voice_search);
+        v_search_txt = findViewById(R.id.home_search_text);
+
+        // bottom views
+        v_home = findViewById(R.id.v_home);
+        v_music = findViewById(R.id.v_music);
+        v_recharge = findViewById(R.id.v_recharge);
+        v_care = findViewById(R.id.v_care);
+        v_menu = findViewById(R.id.v_menu);
+
+        //middle views
+        top_slider = findViewById(R.id.top_slider);
+        top_dot_slider = findViewById(R.id.top_dot_slider);
 
     }
 
